2cea649a33101fbd5dd5f7916d0ca0b1efac81e4
eid <eidalhadidi18@gmail.com>
2025-11-16 18:03:50 +0300

    recreating the project in .NET framework 4.7.2

--- BL/Class1.cs
+++ BL/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace BL
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
 {
     public class Class1
     {
-
     }
 }
---
+++ TestConsole/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is console for .NET framework");
+            Console.ReadKey();
+        }
+    }
+}
