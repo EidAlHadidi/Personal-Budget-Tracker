74ee0fed22128ccdf83e1fd686eff096fdf0b0cc
eid <eidalhadidi18@gmail.com>
2025-11-16 13:10:15 +0300

    Initial commit of desktop app solution

---
+++ TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is the test file for PBT Project");
+            Console.ReadKey();
+        }
+    }
+}
